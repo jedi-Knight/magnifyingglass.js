85395cd5d7e188419205f8e3c0add76229aed5fb
jediKnight <gautamxpratik@gmail.com>
2014-08-30 10:45:39 +0545

    init;

--- glass.js
+++ glass.js
@@ -1,52 +1,52 @@
-function MagnifyingGlass(content){
-    var glass = document.createElement("div");    
+function MagnifyingGlass(content) {
+    var glass = document.createElement("div");
     glass.setAttribute("class", "glass");
-    var style = "width:200px;"+
-        "height:200px;"+
-        "border:#cccccc solid thick;"+
-        "overflow:hidden;"+
-        "border-radius:100px;"+
-        "box-shadow:0px 0px 6px 2px #000000;"+
-        "position:absolute;";
-    glass.setAttribute("style",style);
+    var style = "width:200px;" +
+            "height:200px;" +
+            "border:#cccccc solid thick;" +
+            "overflow:hidden;" +
+            "border-radius:100px;" +
+            "box-shadow:0px 0px 6px 2px #000000;" +
+            "position:absolute;";
+    glass.setAttribute("style", style);
     content = content.cloneNode(true);
-    var zoomStyle = "-webkit-transform-origin: -150px -150px;"+
-        "-webkit-transform:scale(2,2);";
-    
-    this.defaultStyle = function(){
+    var zoomStyle = "-webkit-transform-origin: -150px -150px;" +
+            "-webkit-transform:scale(2,2);";
+
+    this.defaultStyle = function() {
         return style;
     }
     var contentStyle = content.getAttribute("style");
-    contentStyle = contentStyle? contentStyle: "";
-    content.setAttribute("style",contentStyle+zoomStyle);
-    
-    this.defaultContentStyle = function(){
-        return contentStyle+zoomStyle;
+    contentStyle = contentStyle ? contentStyle : "";
+    content.setAttribute("style", contentStyle + zoomStyle);
+
+    this.defaultContentStyle = function() {
+        return contentStyle + zoomStyle;
     }
     glass.appendChild(content);
-    this.getElement = function(){    
+    this.getElement = function() {
         return glass;
     }
 }
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
 
-var target = document.getElementById("container");
-var magnifyingGlass = new MagnifyingGlass(target.getElementsByClassName("zoomee")[0]);
-target.appendChild(magnifyingGlass.getElement());
+    var target = document.getElementById("container");
+    var magnifyingGlass = new MagnifyingGlass(target.getElementsByClassName("zoomee")[0]);
+    target.appendChild(magnifyingGlass.getElement());
 //var contentStyle = target.getElementsByClassName("zoomee")[0].getAttribute("style");
 
-target.addEventListener("mousemove", function(e){
-    var x = (Number(e.clientX));
-    var y = (Number(e.clientY));
-    var x1 = (Number(e.layerX));
-    var y1 = (Number(e.layerY));
-    magnifyingGlass.getElement().setAttribute("style", magnifyingGlass.defaultStyle()+"left:"+(x-100)+"px;top:"+(y-100)+"px;"); 
-    var content = magnifyingGlass.getElement().getElementsByClassName("zoomee")[0];
-    content.setAttribute("style",magnifyingGlass.defaultContentStyle()+"margin-left:"+(-2*x)+"px;margin-top:"+(-2*y)+"px;");
-    
-    document.getElementById("coords").innerHTML= x+", "+y;
-    document.getElementById("coordsOne").innerHTML= x1+", "+y1;
-});
+    target.addEventListener("mousemove", function(e) {
+        var x = (Number(e.clientX));
+        var y = (Number(e.clientY));
+        var x1 = (Number(e.layerX));
+        var y1 = (Number(e.layerY));
+        magnifyingGlass.getElement().setAttribute("style", magnifyingGlass.defaultStyle() + "left:" + (x - 100) + "px;top:" + (y - 100) + "px;");
+        var content = magnifyingGlass.getElement().getElementsByClassName("zoomee")[0];
+        content.setAttribute("style", magnifyingGlass.defaultContentStyle() + "margin-left:" + (-2 * x) + "px;margin-top:" + (-2 * y) + "px;");
+
+        document.getElementById("coords").innerHTML = x + ", " + y;
+        document.getElementById("coordsOne").innerHTML = x1 + ", " + y1;
+    });
 }, false);
 
