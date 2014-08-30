19bc36d6c2a78a726e10137ed3627446fe826c4c
jediKnight <gautamxpratik@gmail.com>
2014-08-30 20:22:00 +0545

    Ready for Alpha Release !!; :D

---
+++ examples/js/glassExample.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    new MagnifyingGlass.MagnifyingGlass({
+        target: this.getElementById("magnifying-glass-target"),
+        opaque: true,
+        power:3,
+        glass_diameter:300
+    }).magnification.start();
+});
